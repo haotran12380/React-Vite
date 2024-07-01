100310d3406e2cbcd979f9882e3b33038f1cc5da
Hao Tran <haotran12380@gmail.com>
2024-07-01 16:18:05 +0700

    video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,8 +1,15 @@
 import './style.css'
 const MyComponent = () => {
+    // const haoTran = `Nu De Tien 1`;
+    // const haoTran = {
+    //     name: `Haotran`,
+    //     age: `24`
+    // }
+    const haoTran = [1, 2, 3, 4]
     return (
         <>
-            <div >Hao Tran</div>
+            <div > {JSON.stringify(haoTran)} Hao Tran</div>
+            <div>{console.log(`Haotran`)}</div>
             <div className='child' style={
                 { borderRadius: `10px` }
             }>child</div>
