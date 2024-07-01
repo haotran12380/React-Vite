3688532655d3ae9254104bfab5beb1fbf841cfa1
Hao Tran <haotran12380@gmail.com>
2024-07-01 16:01:17 +0700

    video 30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,6 +1,12 @@
+import './style.css'
 const MyComponent = () => {
     return (
-        <div>Hao Tran</div>
+        <>
+            <div >Hao Tran</div>
+            <div className='child' style={
+                { borderRadius: `10px` }
+            }>child</div>
+        </>
     );
 }
 export default MyComponent;
