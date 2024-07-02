219b1b99229a9db83bbcf10ac3e1d48be891678a
Hao Tran <haotran12380@gmail.com>
2024-07-02 21:17:41 +0700

    video 35

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,11 +1,18 @@
 const TodoNew = (props) => {
     console.log(`check props: `, props);
     const { addNewTodo } = props;
-    addNewTodo("Hao tran");
+    // addNewTodo("Hao tran");
+    const handleClick = () => {
+        alert(`Click me`)
+    }
+    const handleOnChange = (name) => {
+        console.log(`>> Check on Change`, name);
+    }
     return (
         <div className='todo-new'>
-            <input className='todo-Input' type="text" placeholder='  Enter Your Task' />
-            <button className='btn-Add'>Add</button>
+            <input className='todo-Input' type="text" placeholder='  Enter Your Task'
+                onChange={(event) => handleOnChange(event.target.value)} />
+            <button className='btn-Add' onClick={handleClick}>Add</button>
         </div>
     )
 }
